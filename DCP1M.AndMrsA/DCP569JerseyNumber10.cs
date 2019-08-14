82bc8e439fa7eb8b6b239863e1447a9a5acc80b2
aamir <wasi3aamir@gmail.com>
2019-08-14 22:09:49 +0600

    DCP-16 ...wrong answer

---
+++ DCP1M.AndMrsA/DCP569JerseyNumber10.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace DCP1M.AndMrsA
+{
+    public class DCP569JerseyNumber10
+    {
+        static void Main()
+        {
+            uint T = uint.Parse(Console.ReadLine());
+            for (uint t = 0; t < T; t++)
+            {
+                uint player = uint.Parse(Console.ReadLine());
+                uint max = 0;
+                string name = string.Empty;
+                for (uint p = 1; p <= player; p++)
+                {
+                    string line = Console.ReadLine();
+                    var space = line.Split(' ');
+                    string playerName = space[0];
+                    bool isTrue = playerName.Length < 3 ? true : false;
+                    uint experiencedYear = uint.Parse(space[1]);
+                    if (isTrue)
+                    {
+                        if (max < experiencedYear)
+                        {
+                            max = experiencedYear;
+                            name = playerName;
+                        } 
+                    }
+                }
+                Console.WriteLine($"{name}");
+            }
+        }
+    } 
+}
--- DCP1M.AndMrsA/Program.cs
+++ DCP1M.AndMrsA/Program.cs
@@ -10,6 +10,28 @@ namespace DCP1M.AndMrsA
     {
         static void Main(string[] args)
         {
+            int T = int.Parse(Console.ReadLine());
+            for (int t = 1; t <= T; t++)
+            {
+                string line = Console.ReadLine();
+                var space = line.Split(' ');
+                int num1 = int.Parse(space[0]);
+                int num2 = int.Parse(space[1]);
+                Console.WriteLine($"Case {t}: {CountSquares(num1,num2)}");
+            }
+        }
+        static int CountSquares(int a,int b)
+        {
+            int count = 0;
+            for (int i = a; i <=b; i++)
+            {
+                for (int j = 0; j*j <=i; j++)
+                {
+                    if (j * j == i)
+                        count++;
+                }
+            }
+            return count;
         }
     }
 }
